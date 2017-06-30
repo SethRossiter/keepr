cd9b9a1593af19166de4c8c90320990de0ef255f
Seth Rossiter <strossiter87@gmail.com>
2017-06-29 20:53:05 -0600

    heroku

--- www/src/store/index.js
+++ www/src/store/index.js
@@ -6,13 +6,13 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 let api = axios.create({
-  baseURL: '//seth-keeper.herokuapp.com/api/',
+  baseURL: 'https://seth-keeper.herokuapp.com/api/',
   timeout: 2000,
   withCredentials: true
 })
 
 let auth = axios.create({
-  baseURL: '//seth-keeper.herokuapp.com/',
+  baseURL: 'https://seth-keeper.herokuapp.com/',
   timeout: 2000,
   withCredentials: true
 })
